653e92c8eaf8af0f85abad081c9f047f5a8275c5
eduardoliron <eduardoliron@gmail.com>
2018-06-25 15:03:35 -0300

    cname e ajustes

--- map.js
+++ map.js
@@ -1,11 +1,11 @@
 var mapaDeNavegacao = [
     {name: "Tem Discriminação em São Caetano?", video: "lxTwvKxq6WY", pergunta: [1,2,3]},
     {name: "Tem Capoeira em São Caetano?", video: "u1LrjwM-vVI", pergunta: [2,3,0]},
-    {name: "Tem Memória em São Caetano?", video: "hlwZK8Ulf3M", pergunta: [3,0,1]},
+    {name: "Tem Históiria em São Caetano?", video: "hlwZK8Ulf3M", pergunta: [3,0,1]},
     {name: "Tem Verticalização em São Caetano?", video: "EgzDIbtsFhY", pergunta: [0,1,2]},
     {name: "Tem Cultura Popular em São Caetano?", video: "M7lc1UVf-VE", pergunta: [12,13,14]},
     {name: "Tem Nordestinos em São Caetano?", video: "M7lc1UVf-VE", pergunta: [2,3,0]},
-    {name: "Tem Tradição Oral em São Caetano?", video: "M7lc1UVf-VE", pergunta: [2,3,0]},
+    {name: "Tem Memória em São Caetano?", video: "M7lc1UVf-VE", pergunta: [2,3,0]},
     {name: "Tem Presença Indígena em São Caetano?", video: "M7lc1UVf-VE", pergunta: [2,3,0]},
     {name: "Tem Religiões Afro em São Caetano?", video: "lxTwvKxq6WY", pergunta: [2,3,0]},
     {name: "Tem Racismo em São Caetano?", video: "M7lc1UVf-VE", pergunta: [2,3,0]},
